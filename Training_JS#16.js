94ad2ceeb235bb24350b1974e98a3d933d54039e
Priyansh Kandwal <153702794+priyansh-69@users.noreply.github.com>
2024-02-22 11:00:27 +0530

    Update Training_JS#16.js

--- Training_JS#16.js
+++ Training_JS#16.js
@@ -1,1 +1,17 @@
+// Methods of String object--slice(), substring() and substr()
+function cutIt(arr) {
+  
+  let shortestLength = arr[0].length;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length < shortestLength) {
+      shortestLength = arr[i].length;
+    }
+  }
 
+  
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].slice(0, shortestLength);
+  }
+
+  return arr;
+}
